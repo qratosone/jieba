935ef82eda7d21dd445f844beb49ea6b857ae4cd
The space of Qratosone <qratosone@live.com>
2017-07-07 01:37:25 +0800

    add posseg

---
+++ jieba_posseg.py
@@ -0,0 +1,11 @@
+#encoding=utf-8
+import jieba.posseg as pseg
+file=open("demo.txt")
+word_pseg_dict={}
+for line in file:
+    words=pseg.cut(line)
+    for word,flag in words:
+        if word not in word_pseg_dict:
+            word_pseg_dict[word]=flag
+for key in word_pseg_dict:
+    print key,word_pseg_dict[key]
