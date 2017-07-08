a713bef217f873ba891cb5948d3ed564a70f9489
qratosone <qinxubo@jisuanke.com>
2017-07-08 15:00:05 +0800

    add function

--- arg_parser.py
+++ arg_parser.py
@@ -2,8 +2,20 @@ def open_file(filename):
     file=open(filename)
     return file
 
-
-
+def get_frequency(file):
+    import jieba
+    word_list=[]
+    for line in file:
+        seg_list=jieba.cut(line)
+        word_list.extend(seg_list)
+    word_dict={}
+    for item in word_list:
+        if item not in word_dict:
+            word_dict[item]=1
+        else:
+            word_dict[item]+=1
+    word_dict_list=sorted(word_dict.items(),key=lambda item: item[1],reverse=True)
+    return word_dict_list
 
 
 
